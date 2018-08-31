08be0e7dfc83b6d51c99fced977cb4c1f7539e6c
Higgs <higgs@weidong.local>
2018-08-31 18:34:22 +0800

    commit

--- models/u_net.py
+++ models/u_net.py
@@ -29,10 +29,10 @@ class _DecoderBlock(nn.Module):
     def __init__(self, in_channels, middle_channels, out_channels):
         super(_DecoderBlock, self).__init__()
         self.decode = nn.Sequential(
-            nn.Conv2d(in_channels, middle_channels, kernel_size=3),
+            nn.Conv2d(in_channels, middle_channels, kernel_size=3, padding=1),
             nn.BatchNorm2d(middle_channels),
             nn.ReLU(inplace=True),
-            nn.Conv2d(middle_channels, middle_channels, kernel_size=3),
+            nn.Conv2d(middle_channels, middle_channels, kernel_size=3, padding=1),
             nn.BatchNorm2d(middle_channels),
             nn.ReLU(inplace=True),
             nn.ConvTranspose2d(middle_channels, out_channels, kernel_size=2, stride=2),
---
+++ piwise/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# @Time    : 2018/8/31 13:18
+# @Author  : SeaRobbersAndDuck
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: PyCharm
+
+from .network import *
